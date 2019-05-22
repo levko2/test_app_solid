4b3452dd39973dd26aa5e9a23b306da55f136a34
alexander <Password>
2019-05-22 13:40:36 +0300

    Simple background changer.

---
+++ android/app/src/main/java/com/levkovskiy/test_app_solid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.levkovskiy.test_app_solid;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
